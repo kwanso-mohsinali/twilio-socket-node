219a556efeb9d2d03060328390355342f16949ea
Mohsin Ali <mohsin.naqvi@kwanso.com>
2024-11-21 11:23:12 +0500

    delete index.html and remove extra variables

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { dirname } from 'node:path';
 import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+    res.send('Hello world');
 });
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('Client connected via WebSocket');
+    // Handle Twilio Media Streams data
+    socket.on('message', (data) => {
+        console.log('Audio Stream Data:', data);
+        // Process or analyze the audio packets here
+    });
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 server.listen(3000, () => {
-    console.log('server running at http://localhost:5000');
+    console.log('Socket.IO server listening on ws://localhost:3000');
 });
